a78fc9049635ddc047dec44e9265b122af466fcc
JS <jm.ambrose@proton.me>
2025-10-09 13:09:53 -0500

    .gitignore, first assignment

---
+++ misc/me.js
@@ -0,0 +1,11 @@
+let myName = "Jade Schefter";
+let favoriteColor = "Green";
+let favoriteHobby = "Homelab";
+let favoriteFood = "Sandwich";
+let favoriteAnimal = "Frog"
+
+console.log(myName);
+console.log(favoriteColor);
+console.log(favoriteHobby);
+console.log(favoriteFood);
+console.log(favoriteAnimal);
